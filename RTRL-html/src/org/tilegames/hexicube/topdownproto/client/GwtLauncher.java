be803b33c02adf646766ea24f74a400c3d2edfe8
Charlie Birks <admin@daftgames.net>
2013-03-26 19:42:39 +0000

    Add android and HTML projects

---
+++ RTRL-html/src/org/tilegames/hexicube/topdownproto/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package org.tilegames.hexicube.topdownproto.client;
+
+import org.tilegames.hexicube.topdownproto.Game;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(800, 600);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new Game();
+	}
+}
